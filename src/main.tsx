b2add50d6c1efa83ce188b12960f4bf11df01ba8
cyber-darl <onosdarlington939@gmail.com>
2025-03-26 14:28:37 +0100

    oya na

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+
+// this manifest is used temporarily for development purposes
+const manifestUrl = 'https://cyber-darl/github.io/soft.json';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <App />
+  </TonConnectUIProvider>,
+)
